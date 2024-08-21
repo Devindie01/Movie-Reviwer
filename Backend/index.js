cc6e0e2dffff85532b8a5dbb9cc8cf6e05da0b06
Devindie Samaranayake <gdcsamaranayake2001@gmail.com>
2024-08-21 21:30:31 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,8 @@ import app from "./server.js";
 import mongodb from "mongodb";
 import ReviewsDAO from "./dao/reviewsDAO.js";
 
+//start from here
+
 app.use(bodyParser.json());
 
 const MongoClient = mongodb.MongoClient;
