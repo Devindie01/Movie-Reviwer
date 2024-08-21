a9fe2ced2c2c8745eb58424e8b885a2589c6e3dc
Devindie Samaranayake <89955086+Devindie01@users.noreply.github.com>
2024-08-22 00:51:34 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express"; // Import express
 import bodyParser from "body-parser";
-import app from "./server.js";
 import mongodb from "mongodb";
+import cors from "cors"; // Use import for cors as well
 import ReviewsDAO from "./dao/reviewsDAO.js";
-
-const app = express()
-app.use(cors({
-  origin: ['https://movie-reviwer-frontend.vercel.app'],
-  methods: ['GET', 'POST'],
-  credentials: true,
-}
-  
-));
-app.use(express.json());
-
-
-app.use(bodyParser.json());
+import app from "./server.js";
 
 const MongoClient = mongodb.MongoClient;
 const mongoUsername = process.env.MONGO_USERNAME;
@@ -26,6 +15,15 @@ const encodedPassword = encodeURIComponent(mongoPassword);
 const uri = `mongodb+srv://${mongoUsername}:${encodedPassword}@cluster0.e2myved.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const port = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: ['https://movie-reviwer-frontend.vercel.app'],
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(bodyParser.json());
 
 MongoClient.connect(uri, {
   maxPoolSize: 50,
